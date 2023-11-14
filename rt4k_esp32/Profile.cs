c2d62dbe1950846bfb41c49b93635b651702e991
Adam Zey <guspaz@gmail.com>
2023-11-13 23:53:38 -0500

    Optimize profile checksum

--- rt4k_esp32/Profile.cs
+++ rt4k_esp32/Profile.cs
@@ -14,14 +14,27 @@ namespace rt4k_esp32
             int[] crc_table = { 0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7, 0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef };
             int crc = 0;
 
-            // This is actually faster than a for loop initialized at the end of the header
-            byte[] skippedData = new byte[data.Length - RT4K_PROFILE_HEADER_SIZE];
-            Array.Copy(data, RT4K_PROFILE_HEADER_SIZE, skippedData, 0, skippedData.Length);
+            // Partially unroll loop
+            int dataLen = data.Length;
+            int curbyte;
 
-            foreach (byte b in skippedData)
+            for (int i = RT4K_PROFILE_HEADER_SIZE; i < dataLen; i += 4)
             {
-                crc = crc_table[((crc >> 12) ^ (b >> 4)) & 0x0F] ^ (crc << 4);
-                crc = crc_table[((crc >> 12) ^ (b)) & 0x0F] ^ (crc << 4);
+                curbyte = data[i];
+                crc = crc_table[((crc >> 12) ^ (curbyte >> 4)) & 0x0F] ^ (crc << 4);
+                crc = crc_table[((crc >> 12) ^ (curbyte)) & 0x0F] ^ (crc << 4);
+
+                curbyte = data[i+1];
+                crc = crc_table[((crc >> 12) ^ (curbyte >> 4)) & 0x0F] ^ (crc << 4);
+                crc = crc_table[((crc >> 12) ^ (curbyte)) & 0x0F] ^ (crc << 4);
+
+                curbyte = data[i+2];
+                crc = crc_table[((crc >> 12) ^ (curbyte >> 4)) & 0x0F] ^ (crc << 4);
+                crc = crc_table[((crc >> 12) ^ (curbyte)) & 0x0F] ^ (crc << 4);
+
+                curbyte = data[i+3];
+                crc = crc_table[((crc >> 12) ^ (curbyte >> 4)) & 0x0F] ^ (crc << 4);
+                crc = crc_table[((crc >> 12) ^ (curbyte)) & 0x0F] ^ (crc << 4);
             }
 
             return crc & 0xFFFF;
